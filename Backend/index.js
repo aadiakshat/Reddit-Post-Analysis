c88b342b32c6139ed777ef86022a42d348dd61cc
Adarsh Akshat <adarshakshat111105@gmail.com>
2025-12-09 12:35:01 +0530

    hogya

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Fix CORS - Add this exact middleware
-capp.use(cors({
+app.use(cors({
   origin: function(origin, callback) {
     if (!origin) return callback(null, true);
     
